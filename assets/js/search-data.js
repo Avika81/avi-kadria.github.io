3bf825185e2a7932a63c76d65fdf3ea882d2bf49
Avika81 <avi.kadria3@gmail.com>
2026-09-08 19:20:17 +0000

    Deploying to gh-pages from @ Avika81/avi-kadria.github.io@b136e262754b7070bb7c701f52d5cf4365e17c21 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,20 +30,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/hobbies/";
           },
-        },{id: "news-honored-to-receive-the-clore-scholarship-for-exceptional-phd-students",
-          title: 'Honored to receive the Clore Scholarship for exceptional PhD students.',
-          description: "",
-          section: "News",},{id: "news-new-approximate-distance-oracles-and-their-applications-won-the-best-paper-award-at-isaac-2025",
+        },{id: "news-new-approximate-distance-oracles-and-their-applications-won-the-best-paper-award-at-isaac-2025",
           title: '‘New Approximate Distance Oracles and Their Applications’ won the Best Paper Award at...',
-          description: "",
-          section: "News",},{id: "news-improved-girth-approximation-in-weighted-undirected-graphs-was-accepted-to-sicomp",
-          title: '‘Improved Girth Approximation in Weighted Undirected Graphs’ was accepted to SICOMP.',
-          description: "",
-          section: "News",},{id: "news-faster-algorithms-for-2k-1-stretch-distance-oracles-was-accepted-to-icalp-2026",
-          title: '‘Faster Algorithms for (2k-1)-Stretch Distance Oracles’ was accepted to ICALP 2026.',
-          description: "",
-          section: "News",},{id: "news-two-papers-accepted-to-esa-2026-in-l-aquila",
-          title: 'Two papers accepted to ESA 2026 in L’Aquila!',
           description: "",
           section: "News",},{
       id: 'light-theme',
